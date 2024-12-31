5e9f0e1d681c88aa31debd9f4f355a2b6466d9ba
Sudharsanan K <121513706+sponge-24@users.noreply.github.com>
2024-12-31 10:19:30 +0530

    Print hello world to the screen

--- boot.asm
+++ boot.asm
@@ -2,11 +2,26 @@ ORG 0x7c00
 BITS 16
 
 start:
-    MOV ah,0eh
-    MOV al, 'A'
-    MOV bx, 0
-    int 0x10
+    mov si, message
+    call print
     jmp $
 
+print:
+    mov bx, 0
+.loop: 
+    lodsb 
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'Hello world!', 0
 times 510 - ($ - $$) db 0
 dw 0xAA55
